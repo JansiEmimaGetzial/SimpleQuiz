11e454d8adb92b6ad4c8065dbea2ea1e8e9509ff
Jansi Emima Getzial J <147958707+JansiEmimaGetzial@users.noreply.github.com>
2023-11-13 14:25:30 +0530

    Update SimpleQuiz.py

--- SimpleQuiz.py
+++ SimpleQuiz.py
@@ -8,7 +8,7 @@ def new_game():
         print(key)
         for i in choices[question_no-1]:
             print(i)
-        guess = input("Enter the Choice (A , B , C , D ): ")
+        guess = input("Enter Your Answer (A , B , C , D ): ")
         guess = guess.upper()
         guesses.append(guess)
         correct_guesses += check_answer(questions.get(key),guess)
@@ -71,7 +71,23 @@ questions = {
     "7.Daisy Wheel term belongs to?" : "A",
     "8.Which can be the input and output devices both?" : "B",
     "9.One Nibble has?" : "B",
-    "10.What does an Operating System do?" : "D"
+    "10.What does an Operating System do?" : "D",
+    "11.Highest capacity of the storage are" : "B",
+    "12.Which key of the keyboard is mainly used to cancel the program?" : "D",
+    "13.Computer Moniter is also known as?" : "C",
+    "14.Which among the following cities produces highest e-waste in India?" : "D",
+    "15.Which among the following period is known as the era of second generation computer?" : "A",
+    "16.If you need to cut the contents of MS Word, which command will you give?" : "A",
+    "17.Who among the following has designed the JavaScript programing language?" : "C",
+    "18.Magnetic disks are coated with?" : "B",
+    "19.USB is a _______ storage device." : "B",
+    "20.An IP address is a ______ Number." : "A",
+    "21.The .com used frequently in website url can be expressed aS ____" : "C",
+    "22.The term used to refer to horizontal page orientation?" : "D",
+    "23.What are symbol used to identify items in a list?" : "C",
+    "24.The blinking symbol on the computer screen is called the _____" : "A",
+    "25.Data going into the computer is called?" : "B"
+
 
 }
 
@@ -84,7 +100,27 @@ choices = [["A. Charles Babbage","B. Dennis Ritchie","C. Charlie Babbage","D. Ke
           ["A. Printer","B. Plotter","C. Monitor","D. Scanner"],
           ["A. Scanner","B. Touch screen monitor","C. Speaker","D. Digitizer"],
           ["A. 2 Bits","B. 4 Bits","C. 8 Bits","D. 16 Bits"],
-          ["A. Memory Management","B. File Management","C. Application Management","D. All of the above"]]
+          ["A. Memory Management","B. File Management","C. Application Management","D. All of the above"],
+          ["A. Terabyte", "B. Yottabyte", "C. Zettabyte","D. Exabyte"],
+          ["A. Del Key","B. Enter Key","C. Ins Key","D. Esc Key"],
+          ["A. DVU","B. UVD","C. VDU","D. CCTV"],
+          ["A. Delhi","B. Bengaluru","C. Chennai","D. Mumbai"],
+          ["A. 1982 onwards","B. 1971 to 1980","C. 1976 to 1985","D. 1990 to 2000 "],
+          ["A. Ctrl + X","B. Ctrl + C","C. Ctrl + V","D. Ctrl + Z"],
+          ["A. Rasmus Lerdorf","B. Guido van Rossum","C. Brendan Eich","D. James Gosling"],
+          ["A. Magnesium oxide","B. Iron oxide","C. Sulphur dioxide","D. Nitric oxide"],
+          ["A. Primary","B. Secondary","C. Auxiliary","D. Additional"],
+          ["A. 32-bit","B. 16-bit","C. 8-bit","D. 64-bit"],
+          ["A. Corporation","B. Cooperative","C. Commercial","D. Cordial"],
+          ["A. Portrait","B. Alignment","C. Table","D. Landscape"],
+          ["A. Icons","B. Markers","C. Bullets","D. Graphics"],
+          ["A. Cursor","B. Mouse","C. Logo","D. Hand"],
+          ["A. Algorithm","B. Input","C. Output","D. Calculations"]
+
+
+
+
+          ]
           
         
 
@@ -93,6 +129,11 @@ print("Welcome to my Simple Computer Quiz!")
 
 play=input("Do you want to start the Quiz? (Yes/No) :")
 play = play.upper()
+
+if play == "NO":
+    print("Hope to see you another time! ")
+    quit()
+
 if play == "YES":
     print("Okay lets go!")
     new_game()
@@ -101,8 +142,6 @@ while play_again():
     new_game()
 
 
-if play == "NO":
-    print("Hope to see you another time! ")
-    quit()
+
 
 print("Thanks for attending the Quiz! Byeee!")
